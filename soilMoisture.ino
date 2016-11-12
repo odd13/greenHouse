22a59eb6564fc1ba815691774a1d5b6d4121e4d3
oddie <arcadeoddie@gmail.com>
2016-11-12 21:05:22 +1100

    initial commit

---
+++ soilMoisture.ino
@@ -0,0 +1,22 @@
+/*
+  # Example code for the moisture sensor
+  # Editor     : Lauren
+  # Date       : 13.01.2012
+  # Version    : 1.0
+  # Connect the sensor to the A0(Analog 0) pin on the Arduino board
+  
+  # the sensor value description
+  # 0  ~300     dry soil
+  #      humid soil
+  # ~480     in water
+
+  
+*/
+
+void getMoisture(){
+  
+  Serial.print("Moisture Sensor Value:");
+  Serial.println(analogRead(0));  
+  delay(100);
+  
+}
